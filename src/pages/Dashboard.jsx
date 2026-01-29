558299c20f22ba097717f9b75c35f25df08904b4
Robert Pfaff <robert@robertpfaff.net>
2026-01-29 16:25:31 -0500

    correct minor import statement bug

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,23 @@
+
 import { useEffect } from "react";
 import { toast } from "react-hot-toast";
-// Show guest info toast on mount
-useEffect(() => {
-  if (!localStorage.getItem("wild_oasis_guest_toast_shown")) {
-    toast(
-      "This project includes an authentication component to create an account with username and password. As a guest, we redirected you to the dashboard.",
-      { icon: "👋", duration: 7000 }
-    );
-    localStorage.setItem("wild_oasis_guest_toast_shown", "1");
-  }
-}, []);
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import DashboardFilter from "../features/dashboard/DashboardFilter";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
+
 function Dashboard() {
+  useEffect(() => {
+    if (!localStorage.getItem("wild_oasis_guest_toast_shown")) {
+      toast(
+        "This project includes an authentication component to create an account with username and password. As a guest, we redirected you to the dashboard.",
+        { icon: "👋", duration: 7000 }
+      );
+      localStorage.setItem("wild_oasis_guest_toast_shown", "1");
+    }
+  }, []);
+
   return (
     <>
       <Row type="horizontal">
