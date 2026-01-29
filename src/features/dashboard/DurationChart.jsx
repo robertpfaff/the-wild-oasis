cc1521ea25c804794c7fcf9fdaaccbd8264ea2e1
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2026-01-29 18:25:03 -0500

    Log duration chart stays data for debugging
    Added console log to display duration chart data.

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -203,6 +203,7 @@ const startDataDark = [
 
 function prepareData(startData, stays) {
   // A bit ugly code, but sometimes this is what it takes when working with real data 😅
+  console.log("Duration chart data:", stays);
 
   function incArrayValue(arr, field) {
     return arr.map((obj) =>
