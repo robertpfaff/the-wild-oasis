03776a0592e21e8650c6b911b8eeeb27b102ca0d
Robert Pfaff <robert@robertpfaff.net>
2026-01-29 17:08:28 -0500

    remove useQuery import bug

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import { useQuery } from "@tanstack/react-query";
 import supabase from "../../services/supabase";
 
 export function useRecentStays() {
